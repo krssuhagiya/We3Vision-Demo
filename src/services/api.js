f07c77a8ba841320374b0a3898eaeecf14048a62
krssuhagiya <krssuhagiya@gmail.com>
2025-08-27 12:38:16 +0530

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://we3vision-demo-backend.onrender.com/api'
 });
 
 // Request interceptor
